33dcdf896b1f133c52d6f4a335d7dcbb25bc1b85
sofia g <110646163+sofigo1010@users.noreply.github.com>
2023-05-22 11:28:48 -0600

    kuu

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author Sofia Garcia, Julio Garcia Salas
+ * Folder: Hoja10
+ * Archivo: Main.java
+ * Fecha: 22/05/2023
+ */
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ SupportFunctions.java
@@ -0,0 +1,13 @@
+/**
+ * @author Sofia Garcia, Julio Garcia Salas
+ * Folder: Hoja10
+ * Archivo: SupportFunctions.java
+ * Fecha: 22/05/2023
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SupportFunctions {
+
+}
